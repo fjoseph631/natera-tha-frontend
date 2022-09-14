022b732f7680f746ea2d3416411b243f2b30095c
Your Name <you@example.com>
2022-09-14 17:58:15 -0400

    Added sticky css header and images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
-
+import {ApiService} from './api.service'
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
+import { DOCUMENT } from '@angular/common';
+import * as xml2js from 'xml2js';
+import {TranslateService} from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+  ,styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('fade',
+    [
+      state('void', style({ opacity : 0})),
+      transition(':enter',[ animate(300)]),
+      transition(':leave',[ animate(500)]),
+    ]
+)]
 })
 export class AppComponent {
   title = 'myNewApp';
+  rssData: any;
+  rssChannel: any;
+  rssItems: any;
+  rssDate:any;
+  parser: any = new xml2js.Parser({ strict: false, trim: true });
+
+  constructor(private api:ApiService, private translate:TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en')
+  }
+  goToLink(link: any)
+  {
+    window.open(link, '_blank')
+  }
+  ngOnInit(){
+    this.api.getRssData().subscribe((data: any) =>
+    {
+      this.parser.parseString(data, (err:any , result: any) => {
+        this.rssData = result.RSS;
+        this.rssItems = this.rssData.CHANNEL[0].ITEM
+        this.rssChannel = result.RSS.CHANNEL[0]
+        this.rssDate = new Date(this.rssChannel.PUBDATE)
+        this.rssItems.forEach((val:any) => {
+          if (val["MEDIA:CONTENT"] !== undefined)
+          {
+            val.imgURL = val["MEDIA:CONTENT"][0].$.URL;
+          }
+          console.log(val.imgURL)
+        val.URL = val["ATOM:LINK"][0].$.HREF});
+      });
+
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader
+ {    return new TranslateHttpLoader(http);}
